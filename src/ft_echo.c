266118547684bc85ffc82084aae04122aa53a02c
Ester Rosàs <erosas@MacBook-Air-de-Ester.local>
2024-03-03 15:02:16 +0100

    allegedly only 18 echo mpanic test pending to be solved

--- src/ft_echo.c
+++ src/ft_echo.c
@@ -12,27 +12,53 @@
 
 #include "../inc/minishell.h"
 
+static int	is_nopt(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (s[0] != '-')
+		return (0);
+	else
+	{
+		while (s[++i])
+		{
+			if (s[i] != 'n')
+				return (0);
+		}
+	}
+	return (1);
+}
+
 int	ft_echo(t_cmd *cmd)
 {
 	int		i;
+	int		opt;
 
 	i = 0;
+	opt = 0;
 	if (dbl_len(cmd->args) == 1 && cmd->infile == 0)
 	{
 		printf("\n");
 		return (0);
 	}
-	else if (cmd->args[1] && ft_strcmp(cmd->args[1], "-n") == 0)
+	else if (cmd->args[1] && is_nopt(cmd->args[1]))
+	{
+		opt = 1;
 		i++;
+	}
 	while (cmd->args[++i])
 	{
 		if (ft_strcmp(cmd->args[i], "$?") == 0)
 			cmd->args[i] = ft_itoa(g_exst);
-		ft_putstr_fd(cmd->args[i], cmd->outfile);
-		if (i < dbl_len(cmd->args) - 1)
-			ft_putstr_fd(" ", cmd->outfile);
-		else if (ft_strcmp(cmd->args[1], "-n") != 0)
-			ft_putstr_fd("\n", cmd->outfile);
+		if (!is_nopt(cmd->args[i]))
+		{
+			ft_putstr_fd(cmd->args[i], cmd->outfile);
+			if (i < dbl_len(cmd->args) - 1)
+				ft_putstr_fd(" ", cmd->outfile);
+			else if (!opt)
+				ft_putstr_fd("\n", cmd->outfile);
+		}
 	}
 	if (cmd->outfile > 2)
 		close (cmd->outfile);
