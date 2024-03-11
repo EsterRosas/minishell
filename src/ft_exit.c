69eaf60a12c2b3a4d73ad144e4235bb1ec2a61ec
Ester Rosàs <erosas@MacBook-Air-de-Ester.local>
2024-03-12 00:25:39 +0100

    in progress exit function with args

--- src/ft_exit.c
+++ src/ft_exit.c
@@ -12,11 +12,40 @@
 
 #include "../inc/minishell.h"
 
-void	ft_exit(int print)
+static int	check_num(char *s)
 {
-	if (print == 1)
-		printf("exit\n");
-	g_exst = 0;
+	int	i;
+
+	i = 0;
+	if (s[i] && (s[i] == '+'))
+		i++;
+	while (s[i])
+	{
+		if ((s[i] < '0' || s[i] > '9'))
+		{
+			printf("exit\n");
+			handle_error_opt("exit", s, "numeric argument required");
+			exit (255);
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_exit(int print, char **args)
+{
+	if (dbl_len(args) == 1)
+	{
+		if (print == 1)
+			printf("exit\n");
+		g_exst = 0;
+	}
+	else if (check_num(args[1]) == 0)
+	{
+		if (ch  // comprovar aquí en cas que surti del rang
+		
+	else
+		g_exst = ft_itoa(args[1]);
 	restore_terminal_settings();
 	exit (g_exst);
 }
